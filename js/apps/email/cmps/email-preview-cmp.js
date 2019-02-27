693971da8878e024d7f0525e2cabcfb6fd59256e
Tal Barak <tal63566@gmail.com>
2019-02-27 13:21:13 +0200

    loop in email list to render all the inbox emails. email preview is rendering each email detail.

--- js/apps/email/cmps/email-preview-cmp.js
+++ js/apps/email/cmps/email-preview-cmp.js
@@ -5,19 +5,35 @@
 
 
 export default {
-template:`
+    props: ['email'],
+    template: `
 <section class="email-preview-cmp"> 
     <div class="sender-name">tal barak</div>
-    <div class="subj">this is the subject, view in bold</div>- 
-    <div class="body">body start before the stretch</div>
-    <div class="recived-at">12:45 AM </div>
+    <div class="subj">{{email.subject}}</div>- 
+    <div class="body">{{email.body}}</div>
+    <div class="recived-at">is read: {{email.isRead}}</div>
+    <div class="date">{{formatDate}}</div>
 
 </section>
 `
-,
-data(){
-    return {
-        
+    ,
+    data() {
+        return {
+            date: ''
+        }
+    },
+    computed: {
+        formatDate() {
+            // return this.date = new Date(email.date)
+
+        }
+    },
+
+    created() {
+        console.log(this.email)
+        // this.subject = email.subject
+        // this.body = email.body
+        // this.isRead = email.isRead
+        // this.date = email.date
     }
-}
 }
