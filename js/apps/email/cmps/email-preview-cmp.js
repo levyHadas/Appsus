5d57684fa97a00b838e226b75c08dfa7a2662b8d
Hadas Levy <hadasl789@gmail.com>
2019-02-28 12:45:45 +0200

    fix in filtering nail boxes

--- js/apps/email/cmps/email-preview-cmp.js
+++ js/apps/email/cmps/email-preview-cmp.js
@@ -8,22 +8,22 @@ import {eventBus,EMAILS_UNREAD} from '../../../event-bus.js'
 export default {
     props: ['email', 'isInbox'],
     template: `
-<section class="email-preview-cmp" :class="isRead" @click='readEmail'> 
-    <div class="compressed-mail" v-if="isCompressed">
-        <div class="sender-name" v-if="isInbox">{{email.from}}</div>
-        <div class="sender-name" v-else>{{email.to}}</div>
-        <div class="email-prev-subj">{{email.subject}}</div>
-        <div class="email-prev-time">{{formatDate}}</div>
-        <button id="email-btn" class="btn email-btn btn-info" @click.stop="toogleReadEmail" 
-            v-if="isInbox">toggle read/un-read</button>
-        <button id="email-btn"  class="btn email-btn btn-danger" @click.stop="deleteEmail">Delete</button>
-        <button id="email-btn" class="btn  btn-success" @click.stop>Reply</button>
+        <section class="email-preview-cmp" :class="isRead" @click='readEmail'> 
+            <div class="compressed-mail" v-if="isCompressed">
+                <div class="sender-name" v-if="isInbox">{{email.from}}</div>
+                <div class="sender-name" v-else>{{email.to}}</div>
+                <div class="email-prev-subj">{{email.subject}}</div>
+                <div class="email-prev-time">{{formatDate}}</div>
+                <button id="email-btn" class="btn email-btn btn-info" @click.stop="toogleReadEmail" 
+                    v-if="isInbox">toggle read/un-read</button>
+                <button id="email-btn"  class="btn email-btn btn-danger" @click.stop="deleteEmail">Delete</button>
+                <button id="email-btn" class="btn  btn-success" @click.stop>Reply</button>
 
-    </div>
-    <body-text v-else  :currEmail="email">  </body-text>
+            </div>
+            <body-text v-else  :currEmail="email">  </body-text>
 
 
-</section>
+        </section>
 `
     ,
     data() {
